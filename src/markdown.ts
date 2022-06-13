34fd4d89958f7cbf7b00bb49b1d5ca7dbea0b3bf
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-13 18:24:54 +0800

    fix: changelog generation

--- src/markdown.ts
+++ src/markdown.ts
@@ -2,15 +2,15 @@ import type { GitCommit } from 'changelogen'
 import type { ChangelogOptions } from './types'
 
 export function generateMarkdown(commits: GitCommit[], config: ChangelogOptions) {
-  const typeGroups = groupBy(commits, 'type')
+  const group = groupBy(commits, 'type')
 
   const titlePadding = '&nbsp;&nbsp;&nbsp;'
   let markdown = ''
   const breakingChanges: string[] = []
 
-  for (const type in Object.keys(typeGroups)) {
-    const group = typeGroups[type] || []
-    const lines = group.reverse()
+  for (const type of Object.keys(group)) {
+    const items = group[type] || []
+    const lines = items.reverse()
       .map((commit) => {
         const scope = commit.scope ? `**${commit.scope.trim()}:** ` : ''
         const refs = commit.references.map((r) => {
@@ -32,19 +32,21 @@ export function generateMarkdown(commits: GitCommit[], config: ChangelogOptions)
 
     if (!lines.length)
       continue
-
     if (!config.types[type])
       continue
 
     markdown += `\n\n### ${titlePadding}${config.types[type].title}\n\n${lines.join('\n')}`
   }
 
+  if (!markdown)
+    markdown = '*No significant changes*'
+
   if (breakingChanges.length)
-    markdown = `### ${titlePadding}${config.breakingChangeMessage}\n\n${breakingChanges.join('\n')}\n${markdown}`
+    markdown = `### ${titlePadding}${config.breakingChangeMessage}\n\n${breakingChanges.join('\n')}${markdown}`
 
   const url = `https://github.com/${config.github}/compare/${config.from}...${config.to}`
 
-  markdown += `\n\n<br>\n\n> [Changes on GitHub](${url})\n`
+  markdown += `\n\n> [Changes on GitHub](${url})\n`
 
   return markdown.trim()
 }
