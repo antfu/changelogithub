232383275d566c6767b26ca952d6443cc6f90af4
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-13 23:05:39 +0800

    fix: matching name with dot, close #3

--- src/git.ts
+++ src/git.ts
@@ -3,7 +3,7 @@ import { execa } from 'execa'
 export async function getGitHubRepo() {
   const res = await execa('git', ['config', '--get', 'remote.origin.url'])
   const url = String(res.stdout).trim()
-  const match = url.match(/github\.com[\/:]([\w\d_-]+)\/([\w\d_-]+)(\.git)?$/i)
+  const match = url.match(/github\.com[\/:]([\w\d._-]+)\/([\w\d._-]+)(\.git)?$/i)
   if (!match)
     throw new Error(`Can not parse GitHub repo from url ${url}`)
   return `${match[1]}/${match[2]}`
