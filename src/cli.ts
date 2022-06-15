2e79c72493a95d297cfebf065ba58f80ef872118
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-15 10:57:59 +0800

    feat: use `cac` for cli parser, close #12

--- src/cli.ts
+++ src/cli.ts
@@ -1,69 +1,71 @@
 #!/usr/bin/env node
-import minimist from 'minimist'
 import { blue, bold, cyan, dim, red, yellow } from 'kolorist'
+import cac from 'cac'
 import { version } from '../package.json'
 import { generate, sendRelease } from './index'
 
-const args = minimist(process.argv.slice(2), {
-  boolean: [
-    'draft',
-    'dry',
-    'contributors',
-  ],
-  string: [
-    'token',
-    'github',
-    'from',
-    'to',
-    'name',
-  ],
-  default: {
-    prerelease: undefined,
-  },
-  alias: {
-    draft: 'd',
-  },
-})
+const cli = cac('vitest')
 
-args.token = args.token || process.env.GITHUB_TOKEN
+cli
+  .version(version)
+  .option('-t, --token <path>', 'GitHub Token')
+  .option('--from <ref>', 'From tag')
+  .option('--to <ref>', 'To tag')
+  .option('--github <path>', 'GitHub Repository, e.g. antfu/changelogithub')
+  .option('--name <name>', 'Name of the release')
+  .option('--contributors', 'Show contributors section')
+  .option('--prerelease', 'Mark release as prerelease')
+  .option('-d, --draft', 'Mark release as draft')
+  .option('--capitalize', 'Should capitalize for each comment message')
+  .option('--dry', 'Dry run')
+  .help()
 
-async function run() {
-  console.log()
-  console.log(dim(`changelo${bold('github')} `) + dim(`v${version}`))
+cli
+  .command('')
+  .action(async (args) => {
+    console.log(args)
+    args.token = args.token || process.env.GITHUB_TOKEN
 
-  const { config, md, commits } = await generate(args as any)
+    try {
+      console.log()
+      console.log(dim(`changelo${bold('github')} `) + dim(`v${version}`))
 
-  console.log(bold(config.github))
-  console.log(cyan(config.from) + dim(' -> ') + blue(config.to) + dim(` (${commits.length} commits)`))
-  console.log(dim('--------------'))
-  console.log()
-  console.log(md.replaceAll('&nbsp;', ''))
-  console.log()
-  console.log(dim('--------------'))
+      const { config, md, commits } = await generate(args as any)
 
-  if (config.dry) {
-    console.log(yellow('Dry run. Release skipped.'))
-    return
-  }
+      console.log(bold(config.github))
+      console.log(cyan(config.from) + dim(' -> ') + blue(config.to) + dim(` (${commits.length} commits)`))
+      console.log(dim('--------------'))
+      console.log()
+      console.log(md.replaceAll('&nbsp;', ''))
+      console.log()
+      console.log(dim('--------------'))
 
-  if (!config.to.startsWith('v')) {
-    console.log(yellow(`Current ref "${bold(config.to)}" is not a version tag. Release skipped.`))
-    process.exitCode = 1
-    return
-  }
+      if (config.dry) {
+        console.log(yellow('Dry run. Release skipped.'))
+        return
+      }
 
-  if (!config.token) {
-    console.log(red('No GitHub token found, specify it via GITHUB_TOKEN env. Release skipped.'))
-    process.exitCode = 1
-    return
-  }
+      if (!config.to.startsWith('v')) {
+        console.log(yellow(`Current ref "${bold(config.to)}" is not a version tag. Release skipped.`))
+        process.exitCode = 1
+        return
+      }
 
-  await sendRelease(config, md)
-}
+      if (!config.token) {
+        console.log(red('No GitHub token found, specify it via GITHUB_TOKEN env. Release skipped.'))
+        process.exitCode = 1
+        return
+      }
 
-run()
-  .catch((e) => {
-    console.error(red(String(e)))
-    console.error(dim(e.stack?.split('\n').slice(1).join('\n')))
-    process.exit(1)
+      await sendRelease(config, md)
+    }
+    catch (e: any) {
+      console.error(red(String(e)))
+      if (e?.stack)
+        console.error(dim(e.stack?.split('\n').slice(1).join('\n')))
+      process.exit(1)
+    }
   })
+
+cli.parse()
+
